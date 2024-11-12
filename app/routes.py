e56153484d3e0592f8c8837ed8991c0bf26895e9
ali hussain <ali@gizmo.ai>
2024-11-12 14:30:49 +0000

    Ch 6 finished: not sure I'm committing all the correct files

--- app/routes.py
+++ app/routes.py
@@ -1,3 +1,4 @@
+from datetime import datetime, timezone
 from flask import redirect, render_template, flash, request, url_for
 from app import app
 from app.forms import LoginForm, RegistrationForm
@@ -6,6 +7,7 @@ from urllib.parse import urlsplit
 import sqlalchemy as sa
 from app import db
 from app.models import User
+from app.forms import EditProfileForm
 
 @app.route('/')
 @app.route('/index')
@@ -72,3 +74,24 @@ def user(username):
         {'author': user, 'body': 'Test post #3'}
     ]
     return render_template('user.html', user=user, posts=posts)
+
+@app.before_request
+def before_request():
+    if current_user.is_authenticated:
+        current_user.last_seen = datetime.now(timezone.utc)
+        db.session.commit()
+
+@app.route('/edit_profile', methods=['GET', 'POST'])
+@login_required
+def edit_profile():
+    form = EditProfileForm()
+    if form.validate_on_submit():
+        current_user.username = form.username.data
+        current_user.about_me = form.about_me.data
+        db.session.commit()
+        flash('Your changes have been saved.')
+        return redirect(url_for('edit_profile'))
+    elif request.method == 'GET':
+        form.username.data = current_user.username
+        form.about_me.data = current_user.about_me
+    return render_template('edit_profile.html', title='Edit Profile', form=form)
